3df039f30efa1fadee07f3f99163b313ec513f52
Alexander Zubkov <green@qrator.net>
2024-05-27 11:09:35 +0200

    test sudo

--- t/test1.sh
+++ t/test1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exec -- 2>&1
+
 set -x
 
 docker ps
@@ -7,6 +9,7 @@ docker run -i alpine:latest <<'DOCKER'
 apk add bash iproute2
 bash <<'BASH'
 set -x
+id
 ip netns add a
 ip netns list
 ip -n a link add d0 type dummy
@@ -16,14 +19,21 @@ ip addr show
 BASH
 DOCKER
 
-cat /proc/cpuinfo
-env
-ps -ef
-
 pwd
 ls -lha
 git branch
 git status
+
+sudo bash <<'BASH'
+set -x
+
+#cat /proc/cpuinfo
+lscpu
+kvm-ok
+which qemu
+uname -a
+#env
+#ps -ef
 
 ip netns add a
 ip netns list
@@ -31,3 +41,5 @@ ip -n a link add d0 type dummy
 ip -n a link set d0 up
 ip -n a addr show
 ip addr show
+
+BASH
