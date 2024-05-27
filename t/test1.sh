46049f0eba1543800a3702d9e338241bf630a0c2
Alexander Zubkov <green@qrator.net>
2024-05-27 23:48:04 +0200

    test qemu, privileged docker

--- t/test1.sh
+++ t/test1.sh
@@ -4,20 +4,20 @@ exec -- 2>&1
 
 set -x
 
-#docker ps
-#docker run -i alpine:latest <<'DOCKER'
-#apk add bash iproute2
-#bash <<'BASH'
-#set -x
-#id
-#ip netns add a
-#ip netns list
-#ip -n a link add d0 type dummy
-#ip -n a link set d0 up
-#ip -n a addr show
-#ip addr show
-#BASH
-#DOCKER
+docker ps
+docker run --privileged -i alpine:latest <<'DOCKER'
+apk add bash iproute2
+bash <<'BASH'
+set -x
+id
+ip netns add a
+ip netns list
+ip -n a link add d0 type dummy
+ip -n a link set d0 up
+ip -n a addr show
+ip addr show
+BASH
+DOCKER
 
 pwd
 ls -lha
@@ -32,16 +32,19 @@ apt-get install -y qemu-kvm cpu-checker
 #cat /proc/cpuinfo
 lscpu
 kvm-ok
-which qemu
+which qemu-system-x86_64
+
+timeout 10 qemu-system-x86_64 -accel kvm -m 64M -nographic -vga none
+
 uname -a
 #env
 #ps -ef
 
-ip netns add a
-ip netns list
-ip -n a link add d0 type dummy
-ip -n a link set d0 up
-ip -n a addr show
-ip addr show
+#ip netns add a
+#ip netns list
+#ip -n a link add d0 type dummy
+#ip -n a link set d0 up
+#ip -n a addr show
+#ip addr show
 
 BASH
