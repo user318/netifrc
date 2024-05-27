a0008fa5042cb7f17157d2bfcf1b0939ecbeb385
Alexander Zubkov <green@qrator.net>
2024-05-27 10:30:36 +0200

    add test workflow

---
+++ t/test1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+docker ps
+docker run -i alpine:latest <<'DOCKER'
+apk add bash iproute2
+bash <<'BASH'
+set -x
+ip netns add a
+ip netns list
+ip -n a link add d0 type dummy
+ip -n a link set d0 up
+ip -n a addr show
+ip addr show
+BASH
+DOCKER
+
+cat /proc/cpuinfo
+ps -ef
+pwd
+ls -lha
+env
+
+ip netns add a
+ip netns list
+ip -n a link add d0 type dummy
+ip -n a link set d0 up
+ip -n a addr show
+ip addr show
